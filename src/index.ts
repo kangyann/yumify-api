cec54f2f1e59c5d5bdb7dcc76a1bf11044cc4baf
Kangyann <kangyann727@gmail.com>
2025-12-15 21:25:26 +0700

    chore: new prisma config; feat: update route for transactions

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,9 @@ import PrismaConnect from "../lib/PrismaConnect.js";
 import { AppError, InterfaceAppError } from "../lib/AppError.js";
 import { TypeLanguage, InterfaceResponseByLanguage, InterfaceUserDatabase } from "../interface/api_auth.js";
 import { InterfaceUserCreate } from "../interface/api_create_user.js";
+import { connect } from "http2";
+import { error } from "console";
+import GenerateInvoice from "../lib/GenerateInvoice.js";
 
 const app = express();
 app.use(express.json());
@@ -13,6 +16,7 @@ app.get("/", (_req, res) => {
    res.send("Hello Express!");
 });
 
+// Route AUTH
 app.post("/api/auth", async (req, res): Promise<Response> => {
    if (!req.body || !req.query) {
       return res.status(500).json({ message: "Invalid request." });
@@ -35,7 +39,7 @@ app.post("/api/auth", async (req, res): Promise<Response> => {
    };
 
    try {
-      const finduser: InterfaceUserDatabase = await PrismaConnect.user.findFirstOrThrow({ where: { username } });
+      const finduser: InterfaceUserDatabase = await PrismaConnect.users.findFirstOrThrow({ where: { username } });
       const compare: boolean = await bcrypt.compare(req_password, finduser.password);
       if (!compare) {
          throw new AppError(
@@ -57,6 +61,7 @@ app.post("/api/auth", async (req, res): Promise<Response> => {
    }
 });
 
+// Route Create User
 app.post("/api/users/create", async (req, res): Promise<Response> => {
    if (!req.body) {
       return res.status(500).json({ message: "Invalid request." });
@@ -64,7 +69,7 @@ app.post("/api/users/create", async (req, res): Promise<Response> => {
    try {
       const { email, name, username, password: req_password } = req.body as InterfaceUserCreate;
       const password: string = await bcrypt.hash(req_password, 10);
-      const createuser = await PrismaConnect.user.create({
+      const createuser = await PrismaConnect.users.create({
          data: { email, name, username, password },
          select: { username: true, email: true, createdAt: true },
       });
@@ -74,24 +79,107 @@ app.post("/api/users/create", async (req, res): Promise<Response> => {
    }
 });
 
+// Route Create Product
 app.post("/api/product/create", async (req, res): Promise<Response> => {
    if (!req.body) {
       return res.status(500).json({ message: "Invalid request." });
    }
    const data = req.body;
    try {
-      const createproduct = await PrismaConnect.product.createMany({ data: data });
+      const createproduct = await PrismaConnect.products.createMany({ data: data });
       return res.status(200).json({ message: "OK", data: createproduct });
    } catch (error) {
       return res.status(500).json({ message: "Internal Server Error.", hint: error });
    }
 });
+
+// Route Get Products
 app.get("/api/product", async (req, res): Promise<Response> => {
    try {
-      const products = await PrismaConnect.product.findMany();
+      const products = await PrismaConnect.products.findMany();
+
+      if (!products) {
+         return res.status(404).json({ message: "No products found." });
+      }
+
       return res.status(200).json({ message: "OK", data: products });
    } catch (error) {
       return res.status(500).json({ message: "Internal Server Error.", hint: error });
    }
 });
+
+app.post("/api/transaction", async (req, res): Promise<Response> => {
+   const data: { paymentCode: string; productData: {}[]; totalPriceAll: number } = req.body;
+
+   if (!data || !data.paymentCode || !data.productData || !data.totalPriceAll) {
+      return res.status(400).json({ message: "Invalid request body." });
+   }
+
+   try {
+      const { id: paymentId }: { id: number } = await PrismaConnect.payments.findFirstOrThrow({
+         where: { paymentCode: data.paymentCode },
+         select: { id: true },
+      });
+
+      if (!paymentId) {
+         return res.status(400).json({ message: "Invalid payment code." });
+      }
+
+      const products: {}[] = data.productData;
+      const ProductsTransaction: any[] = [];
+
+      for (const product of products) {
+         const p: { productName: string; productQty: number; totalPrice: number } = product as {
+            productName: string;
+            productQty: number;
+            totalPrice: number;
+         };
+
+         const { id: productsId }: { id: number } = await PrismaConnect.products.findFirstOrThrow({
+            where: { productName: p.productName },
+            select: { id: true },
+         });
+
+         ProductsTransaction.push({ productsId, productQuantity: p.productQty, totalPrice: p.totalPrice });
+      }
+
+      if (ProductsTransaction.length) {
+         const createTransactions = await PrismaConnect.transactions.create({
+            data: {
+               paymentsId: paymentId,
+               invoiceNumber: GenerateInvoice() as string,
+               paymentString: "",
+               status: "PENDING",
+               totalPrice: data.totalPriceAll,
+               productsTransactions: { createMany: { data: ProductsTransaction } },
+            },
+         });
+
+         return res.status(200).json({ message: "Transaction created successfully.", data: createTransactions });
+      }
+
+      return res.status(400).json({ message: "No valid products found in the transaction." });
+   } catch (error) {
+      return res.status(500).json({ message: "Internal Server Error.", hint: error });
+   }
+});
+
+app.get("/api/transaction", async (req, res): Promise<Response> => {
+   const { invoices } = req.query;
+
+   if (!invoices) {
+      return res.status(400).json({ message: "Invoices query is required." });
+   }
+
+   try {
+      const transaction = await PrismaConnect.transactions.findFirstOrThrow({
+         where: { invoiceNumber: invoices as string },
+         include: { productsTransactions: true, paymentId: true },
+      });
+      return res.status(200).json({ transaction });
+   } catch (error) {
+      return res.status(500).json({ message: "Internal Server Error.", hint: error });
+   }
+});
+
 export default app;
